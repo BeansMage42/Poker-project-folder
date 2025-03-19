982260868970ab784e2f85a5615fb681a2ac3f6c
Ryan <foxtrot.dean@gmail.com>
2025-03-19 09:02:13 -0400

    User class pseudocode

--- Poker++/User.cpp
+++ Poker++/User.cpp
@@ -3,5 +3,21 @@ using namespace std;
 #include <iostream>
 class User : public Player {
 public:
-	void Input();
+	void Input()
+	{
+		//if player presses enter
+			//StartGame()
+		//if player presses 1
+			//If player's bet matches previous
+				//Check()
+			//If player's bet does not match previous
+				//Call()
+		//if player presses 2
+			//Fold()
+		//if player presses 3
+			//Input amount
+			//Raise(amount)
+		//if player presses esc
+			//Quit application
+	}
 };
