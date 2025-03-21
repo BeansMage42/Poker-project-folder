10e85368cc215b44b7489d20920966536b2b5b4c
ArmoredNT <zachary.obrien@ontariotechu.net>
2025-03-21 01:00:34 -0400

    Dealer

--- Poker++/Dealer.cpp
+++ Poker++/Dealer.cpp
@@ -1,15 +1,35 @@
 #include "Dealer.h"
 #include <list>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 class Dealer {
 public:
-	void CreateDeck();
-	Card DrawCard();
+	
+	void CreateDeck()
+	{
+		int i = 0;
+
+		for (int s = 0; s < 4; s++)
+		{
+			for (int v = 0; v < 13; v++)
+			{
+				currentDeck.push_back(Card(s, v));
+			}
+		}
+		
+	}
+
+	Card DrawCard()
+	{
+		int index = rand() % currentDeck.size();
+		Card temp = currentDeck.at(index);
+		currentDeck.erase(currentDeck.begin()+ index);
+		
+		return temp;
+	}
 
 private:
-	Card referenceDeck[52];
-	list<Card> currentDeck;
-
-
+	vector<Card> currentDeck;
 
 };
