a87702cb562c28e303cefe3d18f16e84623d9709
Emoral12 <eugbermorales77@gmail.com>
2025-03-21 22:03:52 -0400

    Bot pseudocode added

--- Poker++/Bot.cpp
+++ Poker++/Bot.cpp
@@ -2,11 +2,27 @@ using namespace std;
 #include "Player.h"
 
 class Bot : public Player {
-public:
-	string name;
-	float confidence;
-	float greed;
-
-	void Input();
-	void PickAction();
+	public:
+		string name;
+		float confidence;
+		float greed;
+	
+		void Input();
+		void PickAction()
+		{
+			/*
+			if (confidence too low and greed too low)
+				{
+					Fold()
+				}
+			if (confidence high and greed high)
+				{
+					Raise()
+				}
+			if ((condience high and greed low) or (if confidence low and greed high))
+				{
+					Call()
+				}
+			*/
+		}
 };
